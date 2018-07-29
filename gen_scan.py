78260a5a47c8bb0f608c798a3c875dba9c593795
Aidan Holland <thehappydinoa@gmail.com>
2018-07-28 20:07:51 -0400

    Fixes

--- gen_scan.py
+++ gen_scan.py
@@ -1,36 +1,42 @@
-from scapy import data
 import random
 import string
 
-TCP_REVERSE = dict((scapy.data.TCP_SERVICES[k], k) for k in scapy.data.TCP_SERVICES.keys())
+import scapy.data
+
+TCP_REVERSE = dict((scapy.data.TCP_SERVICES[k], k)
+                   for k in scapy.data.TCP_SERVICES.keys())
+
 
 def find_service(port):
-    if int(port) in TCP_REVERSE:
-        return TCP_REVERSE[int(port)]
-    else:
-        return None
+    port = int(port)
+    if port in TCP_REVERSE:
+        return TCP_REVERSE[port]
+    return
+
 
 def random_service():
     port = random.choice(list(TCP_REVERSE.keys()))
     return (port, TCP_REVERSE[port])
 
+
 def generate_box(subnet=56, hostname="sensenet_"):
     VOWELS = "aeiou"
     CONSONANTS = "".join(set(string.ascii_lowercase) - set(VOWELS))
-    c_ports = [21,22,23,25,53,67,137,138,443]
+    c_ports = [21, 22, 23, 25, 53, 67, 137, 138, 443]
     ports = []
-    for i in range(random.randint(1,10)):
+    for i in range(random.randint(1, 10)):
         if i % 2 == 0:
             hostname += random.choice(CONSONANTS)
         else:
             hostname += random.choice(VOWELS)
-            
-    for i in range(random.randint(1,10)):
-        rc=random.choice(c_ports)
-        ports.append((rc,find_service(rc)))
-    ports = ports+ [random_service() for x in range(random.randint(1,15))]
 
-    return {hostname:{"ports":ports}}
-        
+    for i in range(random.randint(1, 10)):
+        rc = random.choice(c_ports)
+        ports.append((rc, find_service(rc)))
+    ports = ports + [random_service() for x in range(random.randint(1, 15))]
+
+    return {hostname: {"ports": ports}}
+
+
 if __name__ == "__main__":
-    print({"scan":[generate_box() for x in range(random.randint(1,4))]})
+    print({"scan": [generate_box() for x in range(random.randint(1, 4))]})
